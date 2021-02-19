e514cd1967c39add6a7c828365943ad4b5d595bd
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-02-19 14:44:15 +0100

    text

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -25,6 +25,7 @@ const Overview = () => {
                         </Switch>
                     </div>
                 </div>
+
                 <Switch>
                     {Editions.map((edition) => {
                         const editionLink = `/${edition}`;
@@ -35,20 +36,26 @@ const Overview = () => {
                         )
                     })}
                     <Route path="/">
-                    <div className={style.Container}>
-                        {console.log(Editions)}
-                        {Editions.map((edition) => {
-                            const link = `/${edition}`;
-                            return (
-                                <>
-                                    <NavLink className={style.Item} key={link} to={link}>
-                                        {edition}
-                                    </NavLink>
-                                </>
-                            )
-                            })
-                        }
-                    </div>
+                        <div className={style.Intro}>
+                            <h1>Online Sitho magazine</h1>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                        </div>
+                        <div className={style.Container}>
+                            
+                            {console.log(Editions)}
+                            {Editions.map((edition) => {
+                                const link = `/${edition}`;
+                                return (
+                                    <>
+                                    
+                                        <NavLink className={style.Item} key={link} to={link}>
+                                            {edition}
+                                        </NavLink>
+                                    </>
+                                )
+                                })
+                            }
+                        </div>
                     </Route>
                 </Switch>
             </Router>

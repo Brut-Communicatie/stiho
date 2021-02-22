3cb013ca67c822ed167945cd3f8b7b742fb9ba3c
TMB <tmb@TMB-1.local>
2021-02-22 11:54:31 +0100

    ie

--- src/Components/Magazine.js
+++ src/Components/Magazine.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import Iframe from 'react-iframe'
+import React from 'react';
+import Iframe from 'react-iframe';
+import './Magazine.css';
 
 const Magazine = (props) => {
     const edition = props.edition;
     const url = `https://magazine.stiho.nl/${edition}/cover/`;
     return (
     <>
+    <div className="frame">
         <Iframe url={url}
             width="100%"
             height="1080px"
             id="myId"
-            className="myClassname"
+            className="iframe"
             display="initial"
             position="relative"
         />
+    </div>
     </>
     )
 };

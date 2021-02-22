effe6bdb0e3a5ae96dc52b567559efba48abc2df
TMB <tmb@TMB-1.local>
2021-02-22 13:23:10 +0100

    tabs etc

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -18,48 +18,64 @@ const Overview = () => {
             name: 'Magazine 4',
             date: '2020 Maart',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/154584/67MAPSrvkXIPLJHvN_d.png"
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/154584/67MAPSrvkXIPLJHvN_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
         }, 
         {   
             url: 'nr3-2020',
             name: 'Magazine 3',
             date: '2020 Februari',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png"
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
         }, 
         {   
             url: 'nr2-2020',
             name: 'Magazine 2',
             date: '2020 Januari',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/136596/8nrm5bdjt_d.png"
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/136596/8nrm5bdjt_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
         }, 
         {   
             url: 'nr3-2020',
             name: 'Magazine 3',
             date: '2020 Februari',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png"
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
         }, 
         {   
             url: 'nr2-2020',
             name: 'Magazine 2',
             date: '2020 Januari',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/136596/8nrm5bdjt_d.png"
-        },{
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/136596/8nrm5bdjt_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
+        },
+        {
             url: 'nr4-2020',
             name: 'Magazine 4',
             date: '2020 Maart',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/154584/67MAPSrvkXIPLJHvN_d.png"
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/154584/67MAPSrvkXIPLJHvN_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
         }, 
         {   
             url: 'nr3-2020',
             name: 'Magazine 3',
             date: '2020 Februari',
             pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png"
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
+        }, 
+        {   
+            url: 'nr2-2020',
+            name: 'Magazine 2',
+            date: '2020 Januari',
+            pages: '12',
+            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/136596/8nrm5bdjt_d.png",
+            content: "Deze uitgave gaat over een korte introductie over de uitgave."
         }
     ];
 
@@ -91,7 +107,7 @@ const Overview = () => {
                             </div>
                         </div>
                         <div className={style.Intro}>
-                            <h1>Online Sitho magazine</h1>
+                            <h1>Online Stiho magazine</h1>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                         </div>
                         <div className={style.Container}>
@@ -99,11 +115,12 @@ const Overview = () => {
                                 const link = `/${edition.url}`;
                                 return (
                                     <>
-                                        <NavLink key={edition.url} className={style.Item}  to={link}>
+                                        <NavLink target="_blank" key={edition.url} className={style.Item}  to={link}>
                                             <img src={edition.thumbnail} />
                                             <div className={style.Details}>
                                                 <h3>{edition.name}</h3>
                                                 <h4>{edition.date}</h4>
+                                                <p>{edition.content}</p>
                                             </div>
                                         </NavLink>
                                     </>

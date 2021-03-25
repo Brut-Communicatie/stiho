0f4d2be2d2b48b30a07c45e49c37373f1d83fa62
TMB <tmb@TMB-1.local>
2021-03-25 15:30:43 +0100

    api

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import Magazine from './Magazine';
 import {
     BrowserRouter as Router,
@@ -18,34 +18,49 @@ import Facebook from '../Content/facebook-square-brands.svg';
 import { Helmet } from 'react-helmet';
 
 const Overview = () => {
-
-    const Editions = [
-        {
-            url: 'nr4-2020',
-            name: 'Nummer 4',
-            date: 'december 2020',
-            pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/154584/67MAPSrvkXIPLJHvN_d.png",
-            content: "Mondkapjes, niet te veel mensen tegelijk in de keet, extra goed plannen. In dit ‘nieuwe normaal’ doen de zpp’ers in de bouw hun werk. "
-        }, 
-        {   
-            url: 'nr3-2020',
-            name: 'Nummer 3',
-            date: 'oktober 2020',
-            pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/147639/aa43gfdwo_d.png",
-            content: "De bouw traditioneel? Zeker niet, kijk maar naar dit nummer. Zo bouwt BVR Bouw 44 woningen met veel prefab-elementen. En Karbouw bestelde voor een groot project alle materialen online."
-        }, 
-        {   
-            url: 'nr2-2020',
-            name: 'Nummer 2',
-            date: 'juni 2020',
-            pages: '12',
-            thumbnail: "https://s3.eu-west-2.amazonaws.com/screenshots-1d5fh4/136596/8nrm5bdjt_d.png",
-            content: "Vanaf februari kregen we ook in Nederland te maken met het coronavirus. Om de maatregelen kon je ook op jouw Stiho-vestiging niet heen. De dagelijkse bak koffie kon even niet."
+    const [loaded, setLoaded] = useState(false);
+    const [fetching, setFetched] = useState(false);
+    const [title, setTitle] = useState("Laden...");
+    const [introContent, setIntroContent] = useState("Laden..");
+    const [footerTitle, setFooterTitle] = useState("Laden...");
+    const [footerContent, setFooterContent] = useState("Laden...");
+    const [editions, setEditions] = useState(false);
+    
+        useEffect(() => {
+        if (!loaded) {
+            const url = "https://stihoadmin.testjevorm.nl/wp-json/wp/v2/edities?_embed";
+            fetch(url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setEditions(result);
+                    setLoaded(true);
+                });
         }
-    ];
+    }, []);
 
+    useEffect(() => {
+        if (!fetching) {
+            const url = "https://stihoadmin.testjevorm.nl/wp-json/wp/v2/pages/15";
+            fetch(url)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    setTitle(result.acf.titel);
+                    setIntroContent(result.acf.intro_tekst);
+                    setFooterTitle(result.acf.footer_titel);
+                    setFooterContent(result.acf.footer_tekst);
+                    setFetched(true);
+                });
+        }
+    }, []);
+
+
+    console.log(editions);
+
+    if (loaded) {
     return (
         <>
         <Helmet>
@@ -53,12 +68,12 @@ const Overview = () => {
         </Helmet>
             <Router>
                 <Switch>
-                    {Editions.map((edition) => {
-                        console.log(edition);
-                        const link = `/${edition.url}`;
+                    {editions.map((edition) => {
+                        // console.log(edition);
+                        const link = `/${edition.acf.url}`;
                         return (
-                            <Route exact path={link}>
-                                <Magazine edition={edition.url} />
+                            <Route key={link} exact path={link}>
+                                <Magazine edition={edition.acf.url} />
                             </Route>
                         )
                     })}
@@ -75,29 +90,29 @@ const Overview = () => {
                         </div>
                         <div className={style.Intro}>
                             <div className={style.Wrapper}>
-                            <h1>Het Stiho magazine lees je nu (ook) online!</h1>
-                            <p>Al bijna 30 jaar houden we je op de hoogte van de nieuwste ontwikkelingen, mooie projecten en handige producten. Dat deden we sinds 1987 met de Stiho Krant, die we in 2016 grondig gerenoveerd hebben. De krant werd het eigentijdse <a href="https://www.stiho.nl/over-ons/Stiho-Magazine" target="_blank" rel="noopener noreferrer">Stiho Magazine</a>. Sinds begin 2020 publiceren we het Stiho magazine óók online. Je kunt 'm lezen op je mobiel, tablet of je pc. Je kunt er doorheen scrollen, op een product klikken om direct naar de webshop te gaan en artikelen delen met collega’s. Super handig! Het magazine komt vier keer per jaar uit.
+                            <h1>{title}</h1>
+                            <p dangerouslySetInnerHTML={{__html: introContent}}>
                             </p>
                             </div>
                         </div>
                         <div className={style.Container}>
-                            {Editions.map((edition, index) => {
-                                const link = `/${edition.url}`;
+                            {editions.map((edition, index) => {
+                                const link = `/${edition.acf.url}`;
                                 return (
                                     <>
-                                        <NavLink target="_blank" key={edition.url} className={style.Item}  to={link}>
-                                            <img src={edition.thumbnail} />
+                                        <NavLink target="_blank" key={edition.acf.url} className={style.Item}  to={link}>
+                                            <img src={edition.acf.thumbnail} />
                                             <div className={style.Details}>
                                                 {index == 0 ? (
                                                     <>
                                                         <div className={style.Nieuw}>Nieuw</div>
                                                     </>
                                                 ) : null}
-                                                <h3>{edition.name} - {edition.date}</h3>
-                                                <p>{edition.content}</p>
+                                                <h3>{edition.acf.name} - {edition.acf.date}</h3>
+                                                <p>{edition.acf.content}</p>
                                             </div>
                                             <div className={style.Read}>
-                                                <p>Lees {edition.name}</p>
+                                                <p>Lees {edition.acf.name}</p>
                                             </div>
                                         </NavLink>
                                     </>
@@ -108,8 +123,8 @@ const Overview = () => {
                         <div className={style.Footer}>
                             <div className={style.FooterContainer}>
                                 <div className={style.FooterRechts}>
-                                    <h4>Over Stiho</h4>
-                                    <p>Stiho is al sinds 1926 dé groothandel in bouwmaterialen, hout en plaat en levert aan bouwend Nederland. Met 23 vestigingen door heel Nederland is Stiho altijd dichtbij. Binnenkort is dat helemaal het geval, want dan komen er nog 6 vestigingen bij. Op het Stiho-bouwplein vind je Stiho en Giebels – groothandel in ijzerwaren en gereedschappen – samen onder één dak. Zo vind je alles wat je nodig hebt op één plek. Handig!</p>
+                                    <h4>{footerTitle}</h4>
+                                    <p>{footerContent}</p>
                                 </div>
                                 <div className={style.FooterLinks}>
                                     <a href="https://www.instagram.com/stiho_bv/" target="_blank" rel="noopener noreferrer"><img src={Instagram} alt="Instagram" width="25px"/></a>
@@ -122,6 +137,12 @@ const Overview = () => {
                     </Route>
                 </Switch>
             </Router>
+        </>
+    )
+    } 
+    return (
+        <>
+        Ophalen van gegevens..
         </>
     )
 };

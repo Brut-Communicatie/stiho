3637424eb3be724dc306b14cd7cdde985b2b3e2d
Leroy Davidson Descelles <leroy.davidsondescelles@zoolt.com>
2021-07-14 10:48:09 +0200

    ua

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import Overview from './Components/Overview';
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-202225891-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
